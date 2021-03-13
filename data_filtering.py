2a959afc1f756d7bc65a05d96f5fe581c9bb7fb7
Harinarayan14 <62546869+Harinarayan14@users.noreply.github.com>
2021-03-13 17:45:11 +0530

    Update data_filtering.py

--- data_filtering.py
+++ data_filtering.py
@@ -6,13 +6,11 @@ for index, data in enumerate(df["Distance"]):
     if(float(data)>100):
         df.drop(index,inplace=True)
 del(df["Unnamed: 0"])
-df.to_csv("nearer_stars.csv",index=False)
-
-# far stars (150 to 350 stars)
-df2 = pd.read_csv("gravity_data.csv")
-for index, data in enumerate(df2["Distance"]):
+df.reset_index(drop=True,inplace=True)
+print(df)
+# gravity - 150 to 350
+for index, data in enumerate(df["gravity"]):
     if(float(data)<150 or float(data)>350  ):
-        df2.drop(index,inplace=True)
-del(df2["Unnamed: 0"])
-print(df2)
-df2.to_csv("far_stars.csv",index=False)
+        df.drop(index,inplace=True)
+print(df)
+df.to_csv("filtered_stars.csv",index=False)
